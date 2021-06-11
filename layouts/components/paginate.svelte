076459b664ef52d504a90adeb5a3863555b0751e
Roberto Rivera <rzrivera@protonmail.com>
2021-06-11 06:54:32 -0700

    Rename component

--- layouts/components/paginate.svelte
+++ layouts/components/paginate.svelte
@@ -10,7 +10,7 @@
       >
     </li>
     <li class="btn-round w-10 h-10">
-      <a href="post/{currentPage - 1}" class="page-link" aria-label="Previous"
+      <a href="posts/{currentPage - 1}" class="page-link" aria-label="Previous"
         ><span aria-hidden="true">«</span></a
       >
     </li>
@@ -21,7 +21,7 @@
       >
     </li>
     <li class="btn-round w-10 h-10 disabled">
-      <a href="post/{currentPage - 1}" class="page-link" aria-label="Previous"
+      <a href="posts/{currentPage - 1}" class="page-link" aria-label="Previous"
         ><span aria-hidden="true">«</span></a
       >
     </li>
@@ -29,33 +29,33 @@
   {#each Array(totalPages) as _, i}
     {#if currentPage == i + 1}
       <li class="btn-round w-10 h-10 active">
-        <a class="page-link" href="/post/{i + 1}">{i + 1}</a>
+        <a class="page-link" href="/posts/{i + 1}">{i + 1}</a>
       </li>
     {:else}
       <li class="btn-round w-10 h-10">
-        <a class="page-link" href="/post/{i + 1}">{i + 1}</a>
+        <a class="page-link" href="/posts/{i + 1}">{i + 1}</a>
       </li>
     {/if}
   {/each}
   {#if currentPage + 1 <= totalPages}
     <li class="btn-round w-10 h-10">
-      <a href="post/{currentPage + 1}" class="page-link" aria-label="Next"
+      <a href="posts/{currentPage + 1}" class="page-link" aria-label="Next"
         ><span aria-hidden="true">»</span></a
       >
     </li>
     <li class="btn-round w-10 h-10">
-      <a href="post/{totalPages}" class="page-link" aria-label="Last"
+      <a href="posts/{totalPages}" class="page-link" aria-label="Last"
         ><span aria-hidden="true">»»</span></a
       >
     </li>
   {:else}
     <li class="btn-round w-10 h-10 disabled">
-      <a href="post/{currentPage + 1}" class="page-link" aria-label="Next"
+      <a href="posts/{currentPage + 1}" class="page-link" aria-label="Next"
         ><span aria-hidden="true">»</span></a
       >
     </li>
     <li class="btn-round w-10 h-10 disabled">
-      <a href="post/{totalPages}" class="page-link" aria-label="Last"
+      <a href="posts/{totalPages}" class="page-link" aria-label="Last"
         ><span aria-hidden="true">»»</span></a
       >
     </li>
