37ffd0ec81d09f5f3cd65a1bc8a823e491347216
Roberto Rivera <rzrivera@protonmail.com>
2021-06-12 22:44:19 -0700

    Refactor Aside component

---
+++ layouts/components/aside.svelte
@@ -0,0 +1,105 @@
+<script>
+</script>
+
+<aside class="w-full md:w-3/12 px-0">
+  <div class="pl-0 pl-lg-4">
+    <!-- Search -->
+    <div class="row">
+      <h4 class="font-bold"><span>Search</span></h4>
+      <form action="/search" class="widget-search">
+        <input
+          id="search-query"
+          name="s"
+          type="search"
+          placeholder="Type &amp; Hit Enter..."
+        />
+        <button type="submit" class="btn-round" aria-label="Search Post">
+          <i class="icofont-search-2" /></button
+        >
+      </form>
+    </div>
+
+    <!-- Social -->
+    <div class="row">
+      <h4 class="font-bold">Social Links</h4>
+      <ul class="content-meta mb-4">
+        <li class="btn-round inline-block">
+          <a href="#top"><i class="icofont-facebook hover:text-white" /></a>
+        </li>
+        <li class="btn-round inline-block">
+          <a href="#top"><i class="icofont-twitter hover:text-white" /></a>
+        </li>
+        <li class="btn-round inline-block">
+          <a href="#top"><i class="icofont-linkedin hover:text-white" /></a>
+        </li>
+        <li class="btn-round inline-block">
+          <a href="#top"><i class="icofont-github hover:text-white" /></a>
+        </li>
+      </ul>
+    </div>
+
+    <!-- categories -->
+    <div class="row">
+      <h4 class="font-bold"><span>Categories</span></h4>
+      <ul class="content-meta mb-4">
+        <li>
+          <a href="/categories/four-seasons" class="d-flex"
+            >Four seasons
+            <small class="ml-auto">(1)</small></a
+          >
+        </li>
+        <li>
+          <a href="/categories/newyork-city" class="d-flex"
+            >Newyork city
+            <small class="ml-auto">(2)</small></a
+          >
+        </li>
+        <li>
+          <a href="/categories/photobooth" class="d-flex"
+            >Photobooth
+            <small class="ml-auto">(1)</small></a
+          >
+        </li>
+        <li>
+          <a href="/categories/photography" class="d-flex"
+            >Photography
+            <small class="ml-auto">(2)</small></a
+          >
+        </li>
+        <li>
+          <a href="/categories/videography" class="d-flex"
+            >Videography
+            <small class="ml-auto">(1)</small></a
+          >
+        </li>
+      </ul>
+    </div>
+
+    <!-- latest post -->
+
+    <!-- tags -->
+    <div class="row">
+      <h4 class="w-full flex font-bold">Tags</h4>
+      <ul class="content-meta mb-4">
+        <li class="list-inline-item">
+          <a href="/tags/booth">Booth</a>
+        </li>
+        <li class="list-inline-item">
+          <a href="/tags/city">City</a>
+        </li>
+        <li class="list-inline-item">
+          <a href="/tags/image">Image</a>
+        </li>
+        <li class="list-inline-item">
+          <a href="/tags/new">New</a>
+        </li>
+        <li class="list-inline-item">
+          <a href="/tags/photo">Photo</a>
+        </li>
+        <li class="list-inline-item">
+          <a href="/tags/video">Video</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</aside>
--- layouts/content/posts.svelte
+++ layouts/content/posts.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Aside from "../components/aside.svelte";
   export let title, image, body, author, date, categories, tags;
 </script>
 
@@ -7,41 +8,50 @@
 >
   <div class="w-0 md:w-1/12 xl:w-2/12" />
   <div class="w-full md:w-10/12 xl:w-8/12 px-2 md:px-0">
-    <div class="row">
-      <div class="relative rounded-lg overflow-hidden">
-        <img class="" src="/assets/{image.src}" alt={image.alt} />
-		<span class="content-meta">{@html image.reference.replaceAll("<a ", "<a target='blank' rel='noopener noreferrer'")}</span>
+    <div class="row flex flex-wrap">
+      <div class="relative mb-10">
+        <img class="rounded-lg overflow-hidden" src="/assets/{image.src}" alt={image.alt} />
+        <span class="content-meta"
+          >{@html image.reference.replaceAll(
+            "<a ",
+            "<a target='blank' rel='noopener noreferrer'"
+          )}</span
+        >
+      </div>
+      <div class="w-full flex flex-wrap md:w-9/12 mb-5 mb-lg-0 px-0 md:pr-10">
+        <div class="content font-bold text-xl mb-2">
+          <h2 class="h2">{title}</h2>
+        </div>
+        <ul class="content-meta flex flex-wrap mb-4">
+          <li class="px-1">
+            <i class="icofont-user" />
+            <a href={author.url}>{author.name}</a>
+          </li>
+          <li class="px-1">Date : {date}</li>
+          <li class="px-1">
+            Categories:
+            {#each categories as catg, i}
+              <a href="/categories/{catg}" class="content ml-1">
+                {catg}{#if i < categories.length - 1}, {/if}
+              </a>
+            {/each}
+          </li>
+          <li class="px-1">
+            Tags:
+            {#each tags as tag, i}
+              <a href="/tags/{tag}" class="ml-1">
+                {tag}{#if i < tags.length - 1}, {/if}
+              </a>
+            {/each}
+          </li>
+        </ul>
+        <p class="text-base">
+          {@html body}
+        </p>
+      </div>
 
-      </div>
-      <div class="content font-bold text-xl mt-6 mb-2">
-        <h2 class="h2">{title}</h2>
-      </div>
-      <ul class="content-meta flex flex-wrap mb-4">
-        <li class="px-1">
-		  <i class="icofont-user"></i>
-          <a href={author.url}>{author.name}</a>
-        </li>
-        <li class="px-1">Date : {date}</li>
-        <li class="px-1">
-          Categories:
-          {#each categories as catg, i}
-            <a href="/categories/{catg}" class="content ml-1">
-              {catg}{#if i < categories.length - 1}, {/if}
-            </a>
-          {/each}
-        </li>
-        <li class="px-1">
-          Tags:
-          {#each tags as tag, i}
-            <a href="/tags/{tag}" class="ml-1">
-              {tag}{#if i < tags.length - 1}, {/if}
-            </a>
-          {/each}
-        </li>
-      </ul>
-      <p class="text-base">
-        {@html body}
-      </p>
+      <!-- Aside  -->
+      <Aside />
     </div>
   </div>
   <div class="w-0 md:w-1/12 xl:w-2/12" />
