216f75a487df43c42673843f34b8c28e5773a9a4
Roberto Rivera <rzrivera@protonmail.com>
2021-06-18 13:23:26 -0700

    Styles cleanup and staging

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// const production = !process.env.ROLLUP_WATCH;
+
+// module.exports = {
+// 	future: {
+// 	  purgeLayersByDefault: true,
+// 	  removeDeprecatedGapUtilities: true,
+// 	},
+// 	plugins: [
+  
+// 	],
+// 	purge: {
+// 	  content: [
+// 	   "./src/**/*.svelte",
+  
+// 	  ],
+// 	  enabled: production // disable purge in dev
+// 	},
+//   };
