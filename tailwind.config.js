891202b7826c8f76b4724a000b0bb4a84d6fed00
Roberto Rivera <rzrivera@protonmail.com>
2021-06-18 23:49:16 -0700

    Update settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,43 @@
+// Setup boolean to run purge only when not
+// currently in development
 // const production = !process.env.ROLLUP_WATCH;
+const production = false;
 
 // module.exports = {
 // 	future: {
 // 	  purgeLayersByDefault: true,
 // 	  removeDeprecatedGapUtilities: true,
 // 	},
-// 	plugins: [
-  
-// 	],
-// 	purge: {
-// 	  content: [
-// 	   "./src/**/*.svelte",
-  
-// 	  ],
-// 	  enabled: production // disable purge in dev
-// 	},
-//   };
+
+module.exports = {
+  // leverage tailwind built-in css purge feature
+  purge: {
+    content: ["./layouts/**/*.svelte"],
+    enabled: production, // disable purge in dev
+  },
+  // extend tailwind's theme with new color schemes
+  theme: {
+    extend: {
+      colors: {
+        csCool: {
+          primary: "#367ee9",
+          secondary: "#a0aec0",
+          accent: "#718096",
+          background: "#fff",
+          main: "#0d0106",
+          header: "#2d3748",
+        },
+        csSimplicity: {
+          primary: "#367ee9",
+          secondary: "#a0aec0",
+          accent: "#718096",
+          background: "#fff",
+          main: "#0d0106",
+          header: "#2d3748",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
