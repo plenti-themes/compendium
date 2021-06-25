f5fb6f4cfaea4bbc89de2121f942b5a9be78e11c
Roberto Rivera <rzrivera@protonmail.com>
2021-06-24 23:57:38 -0700

    Add projects page and cards component

---
+++ layouts/components/cards_group.svelte
@@ -0,0 +1,61 @@
+<script>
+  export let projArry, uniqProjs, allProjs, projRangeHigh, projRangeLow;
+</script>
+
+{#each uniqProjs as proj, i}
+  <div class="rounded-xl overflow-hidden shadow-md bg-secondary">
+    <div class="flex-none lg:flex">
+      {#if i >= projRangeLow && i < projRangeHigh}
+        <div class="px-6 pt-6 pb-4">
+          {#each allProjs as post, p}
+            {#if proj == post.fields.project}
+              {#if projArry.find((pst) => pst.proj === proj).title == post.fields.title}
+                <h2 class="header pb-2 text-lg md:text-xl lg:text-2xl">Project: {proj}</h2>
+              {/if}
+              <div class="flex items-center py-2">
+                <img
+                  class="inline-block object-cover rounded-md w-20 h-20"
+                  src="/assets/{post.fields.image.src}"
+                  alt={post.fields.image.alt}
+                />
+                <div class="inline-block ml-2">
+                  <h3 class="header mb-1 text-base md:text-lg lg:text-xl">
+                    <a href={post.path}>{post.fields.title}</a>
+                  </h3>
+                  <ul class="text-meta flex flex-wrap">
+                    <li class="pr-2 inline-flex">
+                      <i class="las la-user-astronaut text-base relative" />
+                      <a href={post.fields.author.url}
+                        >{post.fields.author.name}</a
+                      >
+                    </li>
+                    <li class="pr-2 inline-flex">Date : {post.fields.date}</li>
+                  </ul>
+                  <ul class="text-meta flex flex-wrap">
+                    <li class="pr-2 inline-flex">
+                      Categories:
+                      {#each post.fields.categories as catg, i}
+                        <a href="/categories/{catg}" class="ml-1">
+                          {catg}{#if i < post.fields.categories.length - 1},
+                          {/if}
+                        </a>
+                      {/each}
+                    </li>
+                    <li class="pr-2 inline-flex">
+                      Tags:
+                      {#each post.fields.tags as tag, i}
+                        <a href="/tags/{tag}" class="ml-1">
+                          {tag}{#if i < post.fields.tags.length - 1}, {/if}
+                        </a>
+                      {/each}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            {/if}
+          {/each}
+        </div>
+      {/if}
+    </div>
+  </div>
+{/each}
--- layouts/components/projects.svelte
+++ layouts/components/projects.svelte
@@ -1,35 +1,42 @@
 <script>
   import Aside from "../components/aside.svelte";
-  import Cards from "../components/cards.svelte";
+  import Groups from "./cards_group.svelte";
   import Pagination from "../components/paginate.svelte";
 
   export let allContent, title, content;
 
-  let projectsPerPage = 6;
+  let projectsPerPage = 4;
 
   $: currentPage = content.pager;
   let allPosts = allContent.filter((content) => content.type == "posts");
-  let totalPosts = allPosts.length;
-  let totalPages = Math.ceil(totalPosts / projectsPerPage);
-  $: postRangeHigh = currentPage * projectsPerPage;
-  $: postRangeLow = postRangeHigh - projectsPerPage;
+  let allProjs = allPosts.filter((content) => content.fields?.project != "");
+
+  let projArry = [];
+  let projList = [];
+  allProjs.forEach((element) => {
+    projArry.push({ proj: element.fields.project, title: element.fields.title});
+    projList.push(element.fields.project);
+  });
+
+  let uniqProjs = [...new Set(projList)];
+  let totalProjs = uniqProjs.length;
+  let totalPages = Math.ceil(totalProjs / projectsPerPage);
+  $: projRangeHigh = currentPage * projectsPerPage;
+  $: projRangeLow = projRangeHigh - projectsPerPage;
   let allSocial = allContent.filter((content) => content.path == "/")[0].fields
     .theme.social;
 </script>
 
 <div class="w-0 md:w-1/12 xl:w-2/12" />
 <div class="w-full md:w-10/12 xl:w-8/12 px-2 md:px-0">
-<section id="projects">
+  <section id="projects">
     <div class="row md:flex md:flex-wrap">
       <div class="w-full md:w-9/12 mb-5 mb-lg-0 px-0">
-        <div class="text-lg mb-8 text-center">
-          <h2 class="header">{title} Page</h2>
-        </div>
-        <div class="grid grid-cols-1 lg:grid-cols-2 gap-6 md:pr-10">
+        <div class="grid grid-cols-1 gap-6 md:pr-10">
           <!-- ------------------------------------------------------- -->
           <!-- Setup a Card for each post as necessary                 -->
           <!-- ------------------------------------------------------- -->
-          <Cards {allPosts} {postRangeHigh} {postRangeLow} />
+          <Groups {projArry} {uniqProjs} {allProjs} {projRangeHigh} {projRangeLow} />
         </div>
 
         <!-- ------------------------------------------------------- -->
@@ -47,6 +54,6 @@
         <Aside {allPosts} {allSocial} />
       </div>
     </div>
-</section>
+  </section>
 </div>
 <div class="w-0 md:w-1/12 xl:w-2/12" />
