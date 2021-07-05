952c04c4dc50f62ed7ef3410f0ca3571bc23d9b1
Roberto Rivera <Roberto.Rivera@protonmail.com>
2021-07-05 01:07:33 -0700

    Adjust for code highlight and math typesetting

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -17,12 +17,26 @@
 
   <title>{idxContent.title}</title>
 
-  <base href={env ? "/" : env.baseurl} />
+  <base href={env.local ? "/" : env.baseurl} />
 
   <!-- Fix hydration -->
   <script type="module" src="/spa/ejected/main.js"></script>
 
   <link rel="icon" type="image/svg+xml" href="/assets/logo.svg" />
   <link rel="stylesheet" href="/assets/tailwind.global.css" />
+
+  {#if idxContent.theme.codeHighlighting}
+    <link rel="stylesheet" href="/assets/prism.css" />
+  {/if}
+
+  {#if idxContent.theme.mathTypesetting}
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
+      integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc"
+      crossorigin="anonymous"
+    />
+  {/if}
+
   <link rel="stylesheet" href="/spa/bundle.css" />
 </head>
