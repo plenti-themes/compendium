61dbfd270694e829284f2963bc8b7e7f45f4d1ae
roobyz <rzrivera@protonmail.com>
2021-08-18 15:58:03 -0700

    Generalized description for SEO and cards

---
+++ layouts/scripts/get_description.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+  export function get_description(articleBody, desc_length) {
+    let desc_start = articleBody.indexOf("<p>");
+    let desc_end = desc_start + desc_length;
+    let desc =
+      articleBody.substring(
+        desc_start,
+        Math.min(desc_end, articleBody.substring(0, desc_end).lastIndexOf(" "))
+      ) + '<span class="font-bold"> ...</span></p>';
+
+    return desc;
+  }
+</script>
