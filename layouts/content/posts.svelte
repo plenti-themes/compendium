05100aff658e6261c6cd1a83428b214e4ecb4c13
Roberto Rivera <rzrivera@protonmail.com>
2022-01-17 12:09:25 -0800

    Refactor posts to make postmeta DRY

--- layouts/content/posts.svelte
+++ layouts/content/posts.svelte
@@ -1,21 +1,26 @@
 <script>
   // Aside component for search, categories, and tags
   import Aside from "../components/aside.svelte";
+  import PostMeta from "../components/post_meta.svelte";
 
   // Values passed in from "html.svelte"
   export let idxContent, allPosts, tagsList, catgList;
 
   // Content field Values passed in via layout content {...content.fields}
-  export let articleBody,
-    title,
-    image,
-    author,
-    dateCreated,
-    dateModified,
-    categories,
-    tags;
+  export let articleBody, title, image, author;
+  export let dateCreated, dateModified, categories, tags;
+  export let complete = true;
+  export let skipbody = true;
 
   let socialLinks = idxContent.socialLinks;
+  let post = {
+    author_url: author.url,
+    author_name: author.name,
+    dateCreated: dateCreated,
+    dateModified: dateModified,
+    tags: tags,
+    catgs: categories,
+  };
 </script>
 
 <section
@@ -26,47 +31,26 @@
     <div class="row flex flex-wrap">
       <div class="relative mb-8 w-full">
         <img
-          class="rounded-lg overflow-hidden w-full h-screen-70 object-cover object-center"
+          class="rounded-lg overflow-hidden w-full h-screen-60 object-cover object-center"
           src="assets/posts/{image.src}"
           alt={image.alt}
         />
         <span class="text-meta"
           >{@html image.citation.replaceAll(
             "<a ",
-            "<a target='blank' rel='noopener noreferrer'"
+            "<a target='blank' rel='noopener'"
           )}</span
         >
       </div>
       <div class="w-full md:w-9/12 mb-5 sm:mb-0 px-0 md:pr-10">
         <h1 class="header mb-2">{title}</h1>
         <ul class="text-meta flex flex-wrap">
-          <li class="px-1 inline-flex">
-            <i class="las la-user-astronaut text-base relative" />
-            <a href={author.url}>{author.name}</a>
+          <li class="mx-0 -mt-1 text-meta inline-flex">
+            <i class="las la-user-astronaut text-lg" />
           </li>
-          <li class="px-1 inline-flex">Created : {dateCreated}</li>
-          <li class="px-1 inline-flex">Updated : {dateModified}</li>
+          <PostMeta {post} {tagsList} {catgList} {complete} {skipbody} />
         </ul>
-        <ul class="text-meta flex flex-wrap mb-6">
-          <li class="px-1 inline-flex">
-            Categories:
-            {#each categories as catg, i}
-              <a href="catgs/{catgList.indexOf(catg) + 1}" class="ml-1">
-                {catg}{#if i < categories.length - 1},
-                {/if}
-              </a>
-            {/each}
-          </li>
-          <li class="px-1 inline-flex">
-            Tags:
-            {#each tags as tag, i}
-              <a href="tags/{tagsList.indexOf(tag) + 1}" class="ml-1">
-                {tag}{#if i < tags.length - 1}, {/if}
-              </a>
-            {/each}
-          </li>
-        </ul>
-        <p>
+        <p class="mt-6">
           {@html articleBody}
         </p>
       </div>
