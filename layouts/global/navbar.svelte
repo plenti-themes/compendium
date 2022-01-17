d5c31cff2cc589cf3611a40580360b80a859d182
Roberto Rivera <rzrivera@protonmail.com>
2022-01-17 12:20:32 -0800

    Rename navmenu classes

--- layouts/global/navbar.svelte
+++ layouts/global/navbar.svelte
@@ -62,15 +62,15 @@
       <!-- logo -->
       <h1 class="font-bold opacity-100 my-0 flex items-center">
         <img width="50" height="50" src="assets/logo.svg" alt="Logo" />
-        <a class="ml-2 text-4xl menu" href=".">
+        <a class="navmenu ml-2 text-4xl" href=".">
           {#if site_name.length > 1}
             <span class="accent" style="opacity: 1;"
               >{site_name[0].toUpperCase()}</span
-            ><span class="menu" style="border-bottom: 4px solid transparent;"
+            ><span class="navmenu" style="border-bottom: 4px solid transparent;"
               >{site_name[1].toUpperCase()}</span
             >
           {:else}
-            <span class="menu" style="border-bottom: 4px solid transparent;"
+            <span class="navmenu" style="border-bottom: 4px solid transparent;"
               >{idxContent.name}</span
             >
           {/if}
@@ -78,7 +78,7 @@
       </h1>
 
       <!-- hamburger menu -->
-      <div class="menu text-4xl flex md:hidden items-center">
+      <div class="navmenu text-4xl flex md:hidden items-center">
         <button
           id="menu"
           type="button"
@@ -98,15 +98,15 @@
       >
         <div class="text-xl">
           <a class="block md:inline-flex px-2 py-1 md:py-0" href="."
-            ><span class="menu">Home</span></a
+            ><span class="navmenu">Home</span></a
           >
           <a class="block md:inline-flex px-2 py-1 md:py-0" href="projs/"
-            ><span class="menu">Projects</span></a
+            ><span class="navmenu">Projects</span></a
           >
           {#each allPages as page, i}
             {#if page.fields.enabled && page.fields.menu}
               <a class="block md:inline-flex px-2 py-1" href={page.path}
-                ><span class="menu">{page.fields.pageType}</span></a
+                ><span class="navmenu">{page.fields.pageType}</span></a
               >
             {/if}
           {/each}
@@ -115,7 +115,7 @@
             type="button"
             class="inline-block md:inline-flex px-2 py-1 md:py-0"
             on:click={toggleDark}
-            ><span class="menu"><i class="las la-adjust text-2xl" /></span
+            ><span class="navmenu"><i class="las la-adjust text-2xl" /></span
             ></button
           >
         </div>
