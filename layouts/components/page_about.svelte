c948b1a804ab5ab1536913ba3bc81e379edc8299
Roberto Rivera <rzrivera@protonmail.com>
2022-01-17 12:02:31 -0800

    Refactor about page theme

--- layouts/components/page_about.svelte
+++ layouts/components/page_about.svelte
@@ -1,17 +1,27 @@
 <script>
+  import Hoverable from "../scripts/hoverable.svelte";
   export let title, image, hero, splash, articleBody;
+
+  function scrollDown() {
+    window.scrollTo({
+      top: 900,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
 </script>
 
 <!-- Hero section -->
 <div id="hero" class="w-full grid grid-cols-2 relative">
   <div
-    class="pl-2 md:pl-6 lg:pl-10 xl:pl-16 w-screen-30 sm:w-screen-40 md:w-screen-60 lg:w-screen-70 xl:w-screen-80 absolute"
-    style="top: 40%"
+    class="pl-2 md:pl-6 lg:pl-10 xl:pl-16 w-screen-30 sm:w-screen-40 md:w-screen-50 lg:w-screen-60 xl:w-screen-80 absolute bottom-10 sm:bottom-1/4"
   >
     <h2 class="header text-xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
       {hero.pre}<span class="accent-b">{hero.accent}</span><br />{hero.post}
     </h2>
-    <h3 class="tag text-xl sm:text-2xl md:text-4xl">{hero.tagline}</h3>
+    <h3 class="tag text-base sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      {hero.tagline}
+    </h3>
   </div>
   <div />
   <div>
@@ -23,10 +33,24 @@
     <span class="text-meta"
       >{@html image.citation.replaceAll(
         "<a ",
-        "<a target='blank' rel='noopener noreferrer'"
+        "<a target='blank' rel='noopener'"
       )}</span
     >
   </div>
+  <button
+    type="button"
+    on:click={scrollDown}
+    class="absolute bottom-0 sm:bottom-10 md:bottom-20"
+    style="left: 25%; transform: translate(-50%);"
+  >
+    <Hoverable let:hovering>
+      <span class="inline-block {hovering ? 'animate-bounce' : ''}">
+        <i
+          class="accent las la-chevron-circle-down text-5xl lg:text-6xl rounded-full overflow-hidden bg-clip-content opacity-80 hover:opacity-100"
+        />
+      </span>
+    </Hoverable>
+  </button>
 </div>
 
 <!-- About section -->
@@ -40,20 +64,17 @@
     />
   </div>
   <div
-    class="absolute ml-2 md:ml-12 xl:mt-6 bg-black bg-opacity-50 w-screen-45 sm:w-screen-70 xl:w-screen-80 h-screen-70"
+    class="absolute ml-2 md:ml-12 xl:mt-6 bg-black bg-opacity-50 w-screen-45 sm:w-screen-70 lg:w-screen-80 h-screen-70"
     style="top: 14%; overflow-y: auto;"
   >
     <div class="w-full text-lg px-6 pt-10">
       <h2 class="accent text-white text-4xl">{title}</h2>
     </div>
-    <p class="text-white text-md sm:text-lg xl:text-xl px-6 py-6">
+    <p class="text-white text-md sm:text-lg lg:text-xl xl:text-2xl px-6 py-6">
       {@html articleBody}
     </p>
   </div>
 </div>
 <div class="text-meta relative block">
-  {@html splash.citation.replaceAll(
-    "<a ",
-    "<a target='blank' rel='noopener noreferrer'"
-  )}
+  {@html splash.citation.replaceAll("<a ", "<a target='blank' rel='noopener'")}
 </div>
