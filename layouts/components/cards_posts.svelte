9f3eef095fa36e4ed555577eea9b9fa6f9c2d3c5
Roberto Rivera <rzrivera@protonmail.com>
2022-01-17 11:54:15 -0800

    Make postmeta DRY and sort card posts

--- layouts/components/cards_posts.svelte
+++ layouts/components/cards_posts.svelte
@@ -1,9 +1,17 @@
 <script>
-  import Meta from "./post_meta.svelte";
+  import PostMeta from "./post_meta.svelte";
 
   export let allPosts, postRangeHigh, postRangeLow, tagsList, catgList;
   export let complete = true;
   export let skipbody = false;
+
+  // Sort posts in descending order
+  $: allPosts.sort(function (a, b) {
+    return (
+      new Date(b.fields.dateModified).getTime() -
+      new Date(a.fields.dateModified).getTime()
+    );
+  });
 </script>
 
 {#each allPosts as post, i}
@@ -15,10 +23,15 @@
         alt={post.fields.image.alt}
       />
       <div class="px-5 py-4">
-        <h3 class="header mt-0 mb-2 text-xl md:text-2xl">
+        <h3 class="header mt-0 mb-1 text-xl md:text-2xl">
           <a href={post.path}>{post.fields.title}</a>
         </h3>
-        <Meta {post} {tagsList} {catgList} {complete} {skipbody} />
+        <ul class="text-meta flex flex-wrap">
+          <li class="mx-0 -mt-1 text-meta inline-flex">
+            <i class="las la-user-astronaut text-lg" />
+          </li>
+          <PostMeta {post} {tagsList} {catgList} {complete} {skipbody} />
+        </ul>
       </div>
     </div>
   {/if}
