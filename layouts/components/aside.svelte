c1eb7360a504cbc69be5d3171e614f04d445cdfb
Roberto Rivera <rzrivera@protonmail.com>
2022-01-19 09:04:16 -0800

    Refactor for new tag/catg objects with quantities

--- layouts/components/aside.svelte
+++ layouts/components/aside.svelte
@@ -1,9 +1,8 @@
 <script>
-  import { onMount } from "svelte";
   import Search from "../components/search.svelte";
   import Social from "../components/social.svelte";
 
-  export let allPosts, socialLinks, tagsList, catgList;
+  export let allPosts, socialLinks, catgPosts, tagsPosts;
 </script>
 
 <aside class="w-full">
@@ -18,9 +17,9 @@
     <div class="row">
       <h4 class="header mb-1"><span>Categories</span></h4>
       <ul class="text-aside mb-4">
-        {#each catgList as catg, i}
+        {#each Object(catgPosts) as {page, name, length}}
           <li class="flex">
-            <a href="catgs/{i + 1}">{catg}</a>
+            <a href="catgs/{page}">{name} ({length})</a>
           </li>
         {/each}
       </ul>
@@ -30,9 +29,9 @@
     <div class="row">
       <h4 class="header mb-1 w-full flex">Tags</h4>
       <ul class="text-aside mb-4">
-        {#each tagsList as tag, i}
+        {#each Object(tagsPosts) as {page, name, length}}
           <li class="flex">
-            <a href="tags/{i + 1}">{tag}</a>
+            <a href="tags/{page}">{name} ({length})</a>
           </li>
         {/each}
       </ul>
