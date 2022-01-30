8754638ada6d73b52cd6cde4c953748104aed9d7
Roberto Rivera <rzrivera@protonmail.com>
2022-01-30 11:09:29 -0800

    Add initial geo-coding function

---
+++ layouts/scripts/geoCode.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+  export function geo() {
+    // Use fetch method to GET ip data for request options
+    const address = fetch("https://jsonip.com", { mode: "cors" })
+      .then((resp) => resp.json())
+      .then((data) => {
+        return data.ip;
+      });
+
+    return address;
+  }
+</script>
