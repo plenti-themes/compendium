dea3f8e3914a994bbdb3889f8e71efcd8bb31c11
Roberto Rivera <rzrivera@protonmail.com>
2022-02-13 13:42:02 -0800

    Rename function and fix html bugs

--- layouts/components/post_meta.svelte
+++ layouts/components/post_meta.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { get_description } from "../scripts/get_description.svelte";
+  import { get_excerpt } from "../scripts/get_excerpt.svelte";
   export let post, catgPosts, tagsPosts, pm;
 
   let path = post.path;
@@ -7,69 +7,67 @@
   let author_name = post.author_name ?? post.fields.author.name;
   let dateCreated = post.dateCreated ?? post.fields.dateCreated;
   let dateModified = post.dateModified ?? post.fields.dateModified;
+  let title = (post.dateModified ?? post.fields.title) ?? '';
   let tags = post.tags ?? post.fields.tags;
   let catgs = post.catgs ?? post.fields.categories;
   let cardBody = pm.description
-    ? get_description(post.fields.articleBody, 170)
+    ? get_excerpt(post.fields.articleBody, 170)
     : "";
 </script>
 
-{#if pm.author}
-  <li class="mx-0 -mt-1 text-meta inline-flex">
-    <i class="las la-user-astronaut{pm.feature ? ' feature' : ' standard'}" />
-  </li>
-  <li class="mr-2 my-0 inline-flex">
-    <a href={author_url}>{author_name}</a>
-  </li>
-{/if}
-{#if pm.date_created}
-  <li class="mr-2 my-0 inline-flex">
-    Created: {dateCreated}
-  </li>
-{/if}
-{#if pm.date_modified}
-  <li class="mr-2 my-0 inline-flex">
-    Updated: {dateModified}
-  </li>
-{/if}
-{#if pm.catg_tags}
-  <br />
-  <li class="mr-2 my-0 inline-flex">
-    Categories:
-    {#each catgs as catg, i}
-      {#each Object(catgPosts) as { page, name }}
-        {#if catg == name}
-          <a href="catgs/{page}" class="ml-0.5">
-            {name}{#if i < catgs.length - 1},{/if}
-          </a>
-        {/if}
+<ul>
+  {#if pm.author}
+    <li class="mx-0 -mt-1 text-meta inline-flex">
+      <i class="las la-user-astronaut{pm.feature ? ' feature' : ' standard'}" />
+    </li>
+    <li class="mr-2 my-0 inline-flex">
+      <a href={author_url}>{author_name}</a>
+    </li>
+  {/if}
+  {#if pm.date_created}
+    <li class="mr-2 my-0 inline-flex">
+      Created: {dateCreated}
+    </li>
+  {/if}
+  {#if pm.date_modified}
+    <li class="mr-2 my-0 inline-flex">
+      Updated: {dateModified}
+    </li>
+  {/if}
+  {#if pm.catg_tags}
+    <li class="mr-2 my-0 inline-flex">
+      Categories: {#each catgs as catg, i}
+        {#each Object(catgPosts) as { page, name }}
+          {#if catg == name}
+            <a href="catgs/{page}" class="meta ml-0.5">
+              {name}
+            </a>{#if i < catgs.length - 1},{/if}
+          {/if}
+        {/each}
       {/each}
-    {/each}
-  </li>
-  <li class="mr-2 my-0 inline-flex">
-    Tags:
-    {#each tags as tag, i}
-      {#each Object(tagsPosts) as { page, name }}
-        {#if tag == name}
-          <a href="tags/{page}" class="ml-0.5">
-            {name}{#if i < tags.length - 1},{/if}
-          </a>
-        {/if}
+    </li>
+    <li class="mr-2 my-0 inline-flex">
+      Tags: {#each tags as tag, i}
+        {#each Object(tagsPosts) as { page, name }}
+          {#if tag == name}
+            <a href="tags/{page}" class="meta ml-0.5">
+              {name}
+            </a>{#if i < tags.length - 1},{/if}
+          {/if}
+        {/each}
       {/each}
-    {/each}
-  </li>
-{/if}
+    </li>
+  {/if}
+</ul>
 {#if pm.description}
   <p class="text-base mb-8">
     {@html cardBody}
   </p>
 {/if}
 {#if pm.continue}
-  <article class="border-0">
-    <div class="mb-4">
-      <a href={path} class="btn-outline hover:white">Continue Reading</a>
-    </div>
-  </article>
+  <div class="mb-4">
+    <a href={path} title="{title}" class="btn-outline hover:white">Continue Reading</a>
+  </div>
 {/if}
 
 <style>
@@ -99,5 +97,8 @@
       font-size: 1.5rem;
       line-height: 2rem;
     }
+  }
+  .meta {
+    text-decoration: underline;
   }
 </style>
