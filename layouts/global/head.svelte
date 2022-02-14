f603b919267d74c17fce594cea0e955f27fb96af
Roberto Rivera <rzrivera@protonmail.com>
2022-02-13 20:01:28 -0800

    Remove superflous logic

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -8,10 +8,6 @@
 
   // Defne key value for SEO updates
   $: isPost = content.type === "posts" ? true : false;
-
-  // Set flag to enable contact serverless function
-  let enableContact = allPages.filter((key) => key.path === "pages/contact")[0]
-    .fields.enabled;
 </script>
 
 <head>
@@ -29,10 +25,6 @@
 
   <link rel="icon" type="image/svg+xml" href="assets/logo.svg" />
   <link rel="stylesheet" href="assets/tailwind.global.css" />
-
-  {#if enableContact}
-    <!-- Setup contacts assets as needed -->
-  {/if}
 
   {#if idxContent.theme.codeHighlighting}
     <link rel="stylesheet" href="assets/prism.css" />
