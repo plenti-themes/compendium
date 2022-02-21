173ee3d6da0c61a1994ab87ea01119cbf9318e55
Roberto Rivera <rzrivera@protonmail.com>
2022-02-21 07:56:54 -0800

    FIX: excerpt logic

--- layouts/scripts/get_excerpt.svelte
+++ layouts/scripts/get_excerpt.svelte
@@ -1,17 +1,27 @@
 <script context="module">
-    // What: Post excerpt to set as description
-    // Why:  Excerpt artice body to describe what the post is about on
-    //       "complementary" elements and SEO sites 
-    // How:  Select the first paragraph in a post up to specified length
-    export function get_excerpt(articleBody, desc_length) {
-    let desc_start = articleBody.indexOf("<p>");
-    let desc_end = desc_start + desc_length;
-    let desc_text = articleBody.replace(/(<([^>]+)>)/gi, "")
+  // What: Post excerpt to set as description
+  // Why:  Excerpt artice body to describe what the post is about on
+  //       "complementary" elements and SEO sites
+  // How:  Select the first paragraph in a post up to specified length
+  export function get_excerpt(articleBody, desc_length) {
+    // find the first bit of content
+    let desc_start = articleBody.indexOf("<p>")+3;
+
+    // strip out any markup from the excerpt
+    let desc_text = articleBody
+      .substr(desc_start, 300)
+      .replace(/(<([^>]+)>)/gi, "")
+      .substr(0, desc_length);
+
+    // Find the position of the last complete word
+    desc_length = Math.min(
+      desc_length,
+      desc_text.substr(0, desc_length).lastIndexOf(" ")
+    );
+
+    // Trim off any incomplete words and add an ellipsis
     let desc =
-    desc_text.substring(
-        desc_start,
-        Math.min(desc_end, desc_text.substring(0, desc_end).lastIndexOf(" "))
-      ) + '<span class="font-bold"> ...</span>';
+      desc_text.substr(0, desc_length) + '<span class="font-bold"> ...</span>';
 
     return desc;
   }
